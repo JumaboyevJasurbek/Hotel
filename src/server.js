0a7b89bd2c71cf5875dcfe0a1bb90d5a66767a79
JumaboyevJasurbek <100076086+JumaboyevJasurbek@users.noreply.github.com>
2022-11-24 22:30:59 +0500

    add port

--- src/server.js
+++ src/server.js
@@ -5,8 +5,10 @@ import cors from "cors";
 import errorhandler from "./middleware/errorhandler.js";
 dotenv.config();
 
-const app = express();
+const PORT = process.env.PORT || 7000;
 
+const app = express();
+console.log(process.env.NODE_ENV);
 app.use(cors());
 app.use(express.json());
 app.use("/", routes);
@@ -17,4 +19,4 @@ app.all("/*", (req, res) => {
     });
 });
 
-app.listen(7000, console.log(7000));
+app.listen(PORT, console.log(PORT));
